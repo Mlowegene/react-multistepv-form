4a4331be63d12b4dee1d447db9ae67bbc882ff22
Mlowegene <genemlowe@gmail.com>
2023-04-07 00:09:56 +0300

    create sidebar look like the design

--- src/Components/PersonalInfo.jsx
+++ src/Components/PersonalInfo.jsx
@@ -5,15 +5,35 @@ function PersonalInfo() {
   return (
     <div className="body">
       <main className="container">
-        <div>
-          <div>
+        <div className="sidebar">
+          <div className="stepItems">
             <div className="stepNumber">1</div>
-            <p>STEP 1</p>
-            <h1>YOUR INFO</h1>
+            <div>
+              <p className="step">STEP 1</p>
+              <h3 className="stepInfo">YOUR INFO</h3>
+            </div>
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="stepItems">
+            <div className="stepNumber">2</div>
+            <div>
+              <p className="step">STEP 2</p>
+              <h3 className="stepInfo">SELECT PLAN</h3>
+            </div>
+          </div>
+          <div className="stepItems">
+            <div className="stepNumber">3</div>
+            <div>
+              <p className="step">STEP 3</p>
+              <h3 className="stepInfo">ADD-ONS</h3>
+            </div>
+          </div>
+          <div className="stepItems">
+            <div className="stepNumber">4</div>
+            <div>
+              <p className="step">STEP 4</p>
+              <h3 className="stepInfo">SUMMARY</h3>
+            </div>
+          </div>
         </div>
         <div></div>
       </main>
