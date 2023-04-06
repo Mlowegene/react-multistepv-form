404f08c7b91c2311a40591564000af1c12ea83e1
Mlowegene <genemlowe@gmail.com>
2023-04-06 23:22:55 +0300

    app file structure

---
+++ src/Components/PersonalInfo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../css/personalInfo.css";
+
+function PersonalInfo() {
+  return (
+    <div className="body">
+      <main className="container">
+        <div>
+          <div>
+            <div className="stepNumber">1</div>
+            <p>STEP 1</p>
+            <h1>YOUR INFO</h1>
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div></div>
+      </main>
+    </div>
+  );
+}
+
+export default PersonalInfo;
